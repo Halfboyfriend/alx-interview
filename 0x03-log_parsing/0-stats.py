4f289b4d83ac07dcf975b72b8a825dc13ee7cdd9
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-21 00:37:28 +0100

    parsing_log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,51 +2,45 @@
 """
 Write a script that reads stdin line by line and computes metrics
 """
-
-from collections import defaultdict
-import signal
 import sys
 
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-status_codes = {200, 301, 400, 401, 403, 404, 405, 500}
-total_file_size = 0
-lines_by_status_code = defaultdict(int)
-line_count = 0
 
-def print_statistics():
-    print(f"Total file size: File size: {total_file_size}")
-    for status_code in sorted(status_codes):
-        if lines_by_status_code[status_code]:
-            print(f"{status_code}: {lines_by_status_code[status_code]}")
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-        parts = line.split()
-        if len(parts) != 7:
-            continue
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-        _, _, _, _, status_code, file_size, _ = parts
+        stlist = line.split()
+        count += 1
 
         try:
-            status_code = int(status_code)
-            file_size = int(file_size)
-        except ValueError:
-            continue
+            size += int(stlist[-1])
+        except Exception:
+            pass
 
-        if status_code in status_codes:
-            total_file_size += file_size
-            lines_by_status_code[status_code] += 1
-            line_count += 1
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
 
-        if line_count % 10 == 0:
-            print_statistics()
 
 except KeyboardInterrupt:
-    print_statistics()
-    sys.exit(0)
+    printStatus(statusCodes, size)
+    raise
