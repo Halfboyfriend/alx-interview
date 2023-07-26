6325e16cb3a0e09edb81b59ea96dae89847ad888
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-26 18:02:07 +0100

    Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,18 +4,18 @@ Validation
 """
 
 def validUTF8(data):
-    # Helper function to check if the current byte starts with "10"
+    """
+    Return Data"""
     def check_continuation(byte):
         return (byte >> 6) == 0b10
 
     num_bytes_to_process = 0
 
     for byte in data:
-        # Get the 8 least significant bits of the byte
         byte = byte & 0xFF
 
         if num_bytes_to_process == 0:
-            # Determine the number of bytes to process for this character
+            
             if byte >> 7 == 0b0:
                 num_bytes_to_process = 1
             elif byte >> 5 == 0b110:
@@ -27,11 +27,11 @@ def validUTF8(data):
             else:
                 return False
         else:
-            # Check if the current byte starts with "10" (continuation byte)
+            
             if not check_continuation(byte):
                 return False
 
         num_bytes_to_process -= 1
 
-    # If there are remaining bytes to process, it's an invalid encoding
+    
     return num_bytes_to_process == 0
