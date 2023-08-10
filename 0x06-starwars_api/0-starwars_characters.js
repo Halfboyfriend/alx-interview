b4f3686d3634ed08e2841768f8fe2423ca878148
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-10 16:29:11 +0100

    api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,46 @@
 #!/usr/bin/node
 // Write a script that prints all characters of a Star Wars movie:
-const axios = require('axios');
+const request = require('request');
+const { json } = require('stream/consumers');
 
-function getMovieCharacters(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-  
-  axios.get(url)
-    .then(response => {
-      const characters = response.data.characters;
-      
-      characters.forEach(characterUrl => {
-        axios.get(characterUrl)
-          .then(characterResponse => {
-            const characterName = characterResponse.data.name;
-            console.log(characterName);
-          })
-          .catch(error => {
-            console.error(`Failed to retrieve character data for ${characterUrl}`);
-          });
-      });
+async function getMoviesCharacter(id) {
+    const url = `https://swapi.dev/api/films/${id}/`;
+   const data = await request(url, (error, response, body) => {
+        if(error){
+            console.log(`Failed to retrieve movie data for movieId ${id}`)
+            return
+        }
+
+        if (response.statusCode !== 200){
+            console.log(`Failed to retrieve movie data for movieId ${id}`)
+            return
+        }
+
+        const movieDt = JSON.parse(body);
+        const characters = movieDt.characters
+
+
+        characters.forEach(element => {
+            request(element, (CharError, CharReponse, CharBody) => {
+                if(CharError){
+                    console.log(`Failed to retrieve movie data for movieId ${id}`)
+                    return
+                }
+        
+                if (CharReponse.statusCode !== 200){
+                    console.log(`Failed to retrieve movie data for movieId ${id}`)
+                    return
+                }
+
+                const CharacterData = JSON.parse(CharBody);
+                const CharacterName = CharacterData.name
+                console.log(CharacterName)
+            })
+        });
     })
-    .catch(error => {
-      console.error(`Failed to retrieve movie data for Movie ID: ${movieId}`);
-    });
+
+    return data
+
 }
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: node script.js <Movie ID>');
-  process.exit(1);
-}
-
-getMovieCharacters(movieId);
+getMoviesCharacter(3)
