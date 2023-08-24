56ef01a5cea4315c7d90b0424d3172b9f29a4b79
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-24 02:11:04 +0100

    make a change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+"""
+Make is change"""
+import sys
+
 
 def makeChange(coins, total):
-    # Make chanege
+    '''
+    Return: fewest number of coins needed to meet total
+    '''
     if total <= 0:
         return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    # Initialize a list to store the minimum number of coins needed for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
-
-# Example usage
-coins = [1, 2, 5]
-total = 11
-print(makeChange(coins, total))  # Output: 3 (11 = 5 + 5 + 1)
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
