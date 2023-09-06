6fe7d8071262df06a0a2246f6140ae40b9dc3ee5
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-09-06 01:39:41 +0100

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,37 +4,48 @@
 """
 
 def isWinner(x, nums):
-    def isPrime(num):
-        if num < 2:
+    def is_prime(num):
+        if num <= 1:
             return False
-        for i in range(1, int(num**0.5) + 1):
-            if num % i == 0:
+        if num <= 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * i <= num:
+            if num % i == 0 or num % (i + 2) == 0:
                 return False
+            i += 6
         return True
-    
-    def canWin(n):
-        if n == 1:
-            return False
-        
-        for i in range(2, n + 1):
-            if isPrime(i):
-                return True
-        
-        return False
-    
-    Ben = 0
-    Maria = 0
 
-    for j in nums:
-        if canWin(j):
-            if j % 2 == 0:
-                Maria +=1
-            else:
-                Ben += 1
-    
-    if Maria > Ben:
-        return 'Maria'
-    elif Maria < Ben:
+    def play_round(n):
+        if n % 2 == 0:
+            return "Ben"
+        else:
+            return "Maria"
+
+    winner_count = {"Maria": 0, "Ben": 0}
+
+    for i in range(x):
+        n = nums[i]
+        current_player = play_round(n)
+        while n > 1:
+            prime = 2
+            if not is_prime(prime):
+                prime += 1
+            while n % prime != 0:
+                prime += 2
+                while not is_prime(prime):
+                    prime += 2
+            while n % prime == 0:
+                n //= prime
+            current_player = "Maria" if current_player == "Ben" else "Ben"
+
+        winner_count[current_player] += 1
+
+    if winner_count["Maria"] > winner_count["Ben"]:
+        return "Maria"
+    elif winner_count["Ben"] > winner_count["Maria"]:
         return "Ben"
     else:
         return None
